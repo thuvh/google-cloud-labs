a9dfc2fb5f6014d3816b65feed7ce66eaa4871c0
Hoai-Thu Vuong <thuvh87@gmail.com>
2024-04-06 02:37:49 +0700

    GSP315 note

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 1. [Manage Kubernetes in Google Cloud: Challenge Lab][gsp510] - [doc](./gsp510.md) - [script](./gsp510.sh)
 2. [Deploy Kubernetes Applications on Google Cloud: Challenge Lab][gsp318] - [doc](./gsp318.md) - [script](./gsp318.md)
+3. [Set Up an App Dev Environment on Google Cloud: Challenge Lab][gsp315] - [doc][./gsp315.md) - [script](./gsp315.md)
 
 [gsp510]: https://www.cloudskillsboost.google/course_templates/783/labs/408511
 [gsp318]: https://www.cloudskillsboost.google/focuses/10457?parent=catalog#step1
+[gsp315]: https://www.cloudskillsboost.google/focuses/10379?parent=catalog
---
+++ gsp315.sh
@@ -0,0 +1,132 @@
+
+export REGION=us-west1
+export ZONE=us-west1-c
+export PROJECT_ID=qwiklabs-gcp-03-edf32860f2f7
+export BUCKET_NAME=$PROJECT_ID-bucket
+
+gcloud storage buckets create gs://$BUCKET_NAME --location=$REGION
+
+export PUB_SUB_NAME=topic-memories-227
+gcloud pubsub topics create $PUB_SUB_NAME
+
+export FUNC_NAME=memories-thumbnail-creator
+
+cat <<EOF >> index.js
+const functions = require('@google-cloud/functions-framework');
+const crc32 = require("fast-crc32c");
+const { Storage } = require('@google-cloud/storage');
+const gcs = new Storage();
+const { PubSub } = require('@google-cloud/pubsub');
+const imagemagick = require("imagemagick-stream");
+
+functions.cloudEvent('memories-thumbnail-creator', cloudEvent => {
+  const event = cloudEvent.data;
+
+  console.log(`Event: ${event}`);
+  console.log(`Hello ${event.bucket}`);
+
+  const fileName = event.name;
+  const bucketName = event.bucket;
+  const size = "64x64"
+  const bucket = gcs.bucket(bucketName);
+  const topicName = "topic-memories-227";
+  const pubsub = new PubSub();
+  if ( fileName.search("64x64_thumbnail") == -1 ){
+    // doesn't have a thumbnail, get the filename extension
+    var filename_split = fileName.split('.');
+    var filename_ext = filename_split[filename_split.length - 1];
+    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
+    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
+      // only support png and jpg at this point
+      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
+      const gcsObject = bucket.file(fileName);
+      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
+      let gcsNewObject = bucket.file(newFilename);
+      let srcStream = gcsObject.createReadStream();
+      let dstStream = gcsNewObject.createWriteStream();
+      let resize = imagemagick().resize(size).quality(90);
+      srcStream.pipe(resize).pipe(dstStream);
+      return new Promise((resolve, reject) => {
+        dstStream
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+            reject(err);
+          })
+          .on("finish", () => {
+            console.log(`Success: ${fileName} → ${newFilename}`);
+              // set the content-type
+              gcsNewObject.setMetadata(
+              {
+                contentType: 'image/'+ filename_ext.toLowerCase()
+              }, function(err, apiResponse) {});
+              pubsub
+                .topic(topicName)
+                .publishMessage({data: Buffer.from(newFilename)})
+                .then(messageId => {
+                  console.log(`Message ${messageId} published.`);
+                })
+                .catch(err => {
+                  console.error('ERROR:', err);
+                });
+          });
+      });
+    }
+    else {
+      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
+    }
+  }
+  else {
+    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
+  }
+});
+EOF
+
+cat <<EOF >> package.json 
+{
+  "name": "thumbnails",
+  "version": "1.0.0",
+  "description": "Create Thumbnail of uploaded image",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@google-cloud/functions-framework": "^3.0.0",
+    "@google-cloud/pubsub": "^2.0.0",
+    "@google-cloud/storage": "^5.0.0",
+    "fast-crc32c": "1.0.4",
+    "imagemagick-stream": "4.1.1"
+  },
+  "devDependencies": {},
+  "engines": {
+    "node": ">=4.3.2"
+  }
+}
+EOF
+
+export PROJECT_NUMBER=`gcloud projects describe $PROJECT_ID --format='value(projectNumber)'`
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+    --role=roles/eventarc.eventReceiver
+
+export SERVICE_ACCOUNT="$(gsutil kms serviceaccount -p $PROJECT_ID)"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:${SERVICE_ACCOUNT}" \
+    --role='roles/pubsub.publisher'
+
+gcloud functions deploy $FUNC_NAME \
+--gen2 \
+--runtime=nodejs20 \
+--region=$REGION \
+--source=. \
+--allow-unauthenticated \
+--entry-point=$FUNC_NAME \
+--trigger-bucket=gs://$BUCKET_NAME 
+
+curl -O https://storage.googleapis.com/cloud-training/gsp315/map.jpg
+
+gsutil cp map.jpg gs://$BUCKET_NAME
+
+gcloud projects remove-iam-policy-binding $PROJECT_ID --member user:abcxzy
+
